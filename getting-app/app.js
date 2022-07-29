9f6006d104b31490c60773ec208b24ac0dc88724
Ruben Vallejo <ralexanderv89@gmail.com>
2022-07-29 12:31:19 +0200

    feat: adds helmet to repo

--- getting-app/app.js
+++ getting-app/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const helmet = require("helmet");
 const router = require("./routes/index");
 
 mongoose.connect('mongodb://mongo:27017/vizz-importer', {useNewUrlParser: true, useUnifiedTopology:true})
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://mongo:27017/vizz-importer', {useNewUrlParser: true, 
 
 const app = express();
 
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
